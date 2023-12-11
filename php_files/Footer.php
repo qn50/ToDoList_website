82bd9447a284034cab2019bc6ff3c58a38eb968a
Ahmed Qahtan <qahtanqq88@gmail.com>
2023-12-11 17:39:34 +0300

    Add files via upload

--- php_files/Footer.php
+++
@@ -1,6 +0,0 @@
-    <div id="footer">
-        <p>&copy; 2023 Calendary. All rights reserved.</p>
-    </div>
-    <!-- <footer><span style="color: white">&copy; 2023 Calendary Website</span></footer> -->
-    </body>
-</html>
